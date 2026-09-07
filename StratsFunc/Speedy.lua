8b5e1451f247b160f080fda1159b6cb2e57ac69d
AmonguszzZ <sharingminecraft2020@gmail.com>
2026-09-07 20:58:52 +0800

    Create Speedy.lua

---
+++ StratsFunc/Speedy.lua
@@ -0,0 +1,13 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+
+getgenv().AutoMedic = true
+getgenv().AutoMercenary = true
+getgenv().AutoDJ  = true
+getgenv().AutoGatling = true
+
+getgenv().AutoSkip = false
+getgenv().AutoReady = false
+getgenv().AutoRejoin = true
+getgenv().AutoRestart = false
+getgenv().Gatlify = false
+
