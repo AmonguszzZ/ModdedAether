75aa3797e74cb5660c00f9d2e5ec37b4353c5528
AmonguszzZ <sharingminecraft2020@gmail.com>
2026-09-07 06:27:01 -0700

    Update Jailed.lua

--- StratsFunc/Jailed.lua
+++ StratsFunc/Jailed.lua
@@ -7,8 +7,9 @@ getgenv().AutoGatling = true
 
 getgenv().AutoSkip = false
 getgenv().AutoReady = false
-getgenv().AutoRejoin = false
+getgenv().AutoRejoin = true
 getgenv().AutoRestart = false
+getgenv().Gatlify = false
 
 TDS:Loadout("Assassin", "Scout", "Paintballer", "DJ Booth", "Gatling Gun")
 TDS:Mode("Trial")
