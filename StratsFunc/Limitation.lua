4eaa5d13f6357bc5a53dab7f0733b95b01951567
AmonguszzZ <sharingminecraft2020@gmail.com>
2026-09-07 06:27:12 -0700

    Update Limitation.lua

--- StratsFunc/Limitation.lua
+++ StratsFunc/Limitation.lua
@@ -7,8 +7,9 @@ getgenv().AutoGatling = true
 
 getgenv().AutoSkip = false
 getgenv().AutoReady = false
-getgenv().AutoRejoin = false
+getgenv().AutoRejoin = true
 getgenv().AutoRestart = false
+getgenv().Gatlify = false
 
 TDS:Loadout("Trapper", "Gatling Gun", "DJ Booth", "Medic", "Mercenary Base")
 TDS:Mode("Trial")
