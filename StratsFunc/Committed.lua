5a76e3138295f3cf34e99ac39114cfcba912d62c
AmonguszzZ <sharingminecraft2020@gmail.com>
2026-09-07 06:25:15 -0700

    Update Committed.lua

--- StratsFunc/Committed.lua
+++ StratsFunc/Committed.lua
@@ -7,7 +7,7 @@ getgenv().AutoGatling = true
 
 getgenv().AutoSkip = false
 getgenv().AutoReady = false
-getgenv().AutoRejoin = false
+getgenv().AutoRejoin = true
 getgenv().AutoRestart = false
 
 TDS:Loadout("Hacker", "Demoman", "Gatling Gun", "Scout", "Medic")
