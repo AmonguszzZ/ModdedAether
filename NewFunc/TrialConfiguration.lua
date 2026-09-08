0f1367f85fc8f7f5be89fc44fb2f73129ee9a926
AmonguszzZ <sharingminecraft2020@gmail.com>
2026-09-08 08:10:38 -0700

    Update TrialConfiguration.lua

--- NewFunc/TrialConfiguration.lua
+++ NewFunc/TrialConfiguration.lua
@@ -95,7 +95,7 @@ return {
                 ["Bandages"] = 25,
                 ["Accelerator"] = 25,
                 ["Enhanced Optics"] = 20,
-                ["Scavenger"] = 25,
+                ["Scavenger"] = 20,
 				["Improved Gunpowder"] = 25,
 				["Fight Dirty"] = 25,
 				["Precision"] = 15,
